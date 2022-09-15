7a1c6765dfa69c691f5fa903fd1b3a9030c9bd48
Laimne <107036125+Laimne@users.noreply.github.com>
2022-09-15 13:01:29 +0300

    change

--- express/src/App.jsx
+++ express/src/App.jsx
@@ -1,40 +1,15 @@
 import './App.scss';
-import Buttons from './Buttons';
-import { useEffect, useState } from 'react';
-import randColor from './Functions/randColor';
+import Squere from './Squere';
 
 function App() {
-
-    const [number, setNumber] = useState(0);
-    const [color, setColor] = useState(null);
-
-    useEffect(() => {
-        setColor(randColor());
-    }, [number]);
-
-
-
-    useEffect(() => {
-        const timerId = setInterval(() => {
-            setNumber(n => n + 1);
-        }, 100);
-        return () => {
-            clearInterval(timerId);
-        }
-    }, []);
-
-
-    return (
-        <div className="App">
-            <header className="App-header">
-            <h2 style={{color}}>{number}</h2>
-            <h1>Total Recall 2</h1>
-            <Buttons setNumber={setNumber} color="red" t={0} c={100}/>
-            <Buttons setNumber={setNumber} color="green" t={100} c={20} />
-            <Buttons setNumber={setNumber} color="blue" t={120}  c={5}/>
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>T1 DONE</h1>
+        <Squere color="crimson" />
+      </header>
+    </div>
+  );
 }
 
 export default App;
