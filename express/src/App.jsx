9716b100764bd458f9df63665d9b18738eae9e6a
Laimne <107036125+Laimne@users.noreply.github.com>
2022-09-19 11:40:37 +0300

    change

--- express/src/App.jsx
+++ express/src/App.jsx
@@ -1,15 +1,32 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.scss';
-import Squere from './Squere';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>T1 DONE</h1>
-        <Squere color="crimson" />
-      </header>
-    </div>
-  );
+
+    const [trees, setTrees] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3003/trees')
+        .then(res => {
+            setTrees(res.data);
+        })
+    }, []);
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+            <h1>Server</h1>
+            <ul>
+            {
+                trees.map(t => <li key={t.id}>{t.title} <i>{t.height}m</i></li>)
+            }
+            </ul>
+            </header>
+        </div>
+    );
 }
 
 export default App;
